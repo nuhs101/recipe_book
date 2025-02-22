97f2f500c2fae3f348223233536dcb6662ce2203
“Staxupgit config --global user.email “nuhshekhey@gmail.com <nuhshekhey@gmail.com>
2025-02-21 21:36:54 -0500

    favorite button details sc

--- lib/details_screen.dart
+++ lib/details_screen.dart
@@ -1,14 +1,41 @@
 import 'package:flutter/material.dart';
 
-class DetailsScreen extends StatelessWidget {
+class DetailsScreen extends StatefulWidget {
   final Map<String, String> recipe;
+  final Function(Map<String, String>) onFavoriteToggle;
+  final bool isFavorite;
 
-  const DetailsScreen({super.key, required this.recipe});
+  const DetailsScreen({
+    super.key,
+    required this.recipe,
+    required this.onFavoriteToggle,
+    required this.isFavorite,
+  });
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  late bool _isFavorite;
+
+  @override
+  void initState() {
+    super.initState();
+    _isFavorite = widget.isFavorite;
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      _isFavorite = !_isFavorite;
+    });
+    widget.onFavoriteToggle(widget.recipe);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(recipe['name']!)),
+      appBar: AppBar(title: Text(widget.recipe['name']!)),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
@@ -19,7 +46,15 @@ class DetailsScreen extends StatelessWidget {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
             SizedBox(height: 10),
-            Text(recipe['details']!, style: TextStyle(fontSize: 16)),
+            Text(widget.recipe['details']!, style: TextStyle(fontSize: 16)),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _toggleFavorite,
+              child: Text(_isFavorite ? 'Unfavorite' : 'Add to Favorites'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: _isFavorite ? Colors.red : Colors.green,
+              ),
+            ),
           ],
         ),
       ),
